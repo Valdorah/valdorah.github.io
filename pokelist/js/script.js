e57f88c235ad5187d7a20993d2bada0718f8e47e
Valdorah Givrevent <alexisgoncalves1811@gmail.com>
2019-01-26 10:06:07 +0100

    Pokelist - Affichage
    Amélioration de l'affichage des boutons et des sections.

--- pokelist/js/script.js
+++ pokelist/js/script.js
@@ -1,15 +1,14 @@
 var contenu = document.getElementById('contenu');
-var navigationElt = document.createElement('span');
 
 function fillPage(url){
     ajaxGet(url, response => {
     var page = JSON.parse(response);
     page.results.forEach(pokemon => {
-        var pokeDiv = document.createElement('div');
+        var pokeDiv = document.createElement('section');
         var pokeName = document.createElement('p');
         var imgElt = document.createElement('img');
         var types = document.createElement('div');
-        var description = document.createElement('p');
+        var text = document.createElement('p');
         var find = false;
 
         pokeDiv.setAttribute('class', 'nes-container with-title');
@@ -31,7 +30,7 @@ function fillPage(url){
                 var details = JSON.parse(response_3);
                 details.flavor_text_entries.forEach(element => {
                     if(element.language.name == 'en' && find == false){
-                        description.textContent = element.flavor_text;
+                        text.textContent = element.flavor_text;
                         find = true;
                     }
                 });
@@ -48,21 +47,21 @@ function fillPage(url){
         pokeDiv.appendChild(pokeName);
         pokeDiv.appendChild(imgElt);
         pokeDiv.appendChild(types);
-        pokeDiv.appendChild(description);
+        pokeDiv.appendChild(text);
 
         contenu.appendChild(pokeDiv);
         });
 
+        var navigationElt = document.createElement('div');
+        navigationElt.setAttribute('class', 'buttonsPage');
+
         if(page.previous != null){
             var buttonPrevious = document.createElement('button');
-            var lienPrevious = document.createElement('a');
 
-            lienPrevious.setAttribute('href', page.previous);
             buttonPrevious.setAttribute('class', 'nes-btn');
             buttonPrevious.textContent = 'Previous page';
-            lienPrevious.appendChild(buttonPrevious);
 
-            contenu.appendChild(lienPrevious);
+            navigationElt.appendChild(buttonPrevious);
 
             buttonPrevious.addEventListener('click', function (e) {
                 e.preventDefault();
@@ -73,14 +72,11 @@ function fillPage(url){
         
         if(page.next != null){
             var buttonNext = document.createElement('button');
-            var lienNext = document.createElement('a');
 
-            lienNext.setAttribute('href', page.next);
             buttonNext.setAttribute('class', 'nes-btn');
             buttonNext.textContent = 'Next page';
-            lienNext.appendChild(buttonNext);
 
-            contenu.appendChild(lienNext);
+            navigationElt.appendChild(buttonNext);
 
             buttonNext.addEventListener('click', function (e) {
                 e.preventDefault();
@@ -88,6 +84,8 @@ function fillPage(url){
                 fillPage(page.next);
             });
         }
+
+        contenu.appendChild(navigationElt);
     });
 }
 
